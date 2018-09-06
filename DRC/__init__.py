4b22f396e4ae8ff64f5e3f4588e00a74b3cb05f4
NingAnMe <ninganme@qq.com>
2018-09-06 17:39:06 +0800

    修改init

--- DRC/__init__.py
+++ DRC/__init__.py
@@ -8,3 +8,4 @@ from pb_drc_MERSI import ReadMersiL1
 from pb_drc_MODIS import ReadModisL1
 from pb_drc_VIIRS import ReadViirsL1
 from pb_drc_VIRR import ReadVirrL1
+from pb_drc_MVISR import ReadMvisrL1
