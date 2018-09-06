68c6ac37b234806cf7806c396bceb14ccfbcf14c
NingAnMe <ninganme@qq.com>
2018-09-06 14:22:04 +0800

    添加init加载的类

--- DRC/__init__.py
+++ DRC/__init__.py
@@ -1,3 +1,10 @@
 """
 Sat Data Class
 """
+from pb_drc_CRIS import ReadCrisL1
+from pb_drc_IASI import ReadIasiL1
+from pb_drc_IRAS import ReadIrasL1
+from pb_drc_MERSI import ReadMersiL1
+from pb_drc_MODIS import ReadModisL1
+from pb_drc_VIIRS import ReadViirsL1
+from pb_drc_VIRR import ReadVirrL1
